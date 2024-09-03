f82efe4b780c4fa269ca7c0857fb7f2755cd26c8
daisygsoto <dgsoto1@buffs.wtamu.edu>
2024-09-02 21:11:35 -0500

    Add files via upload
    Upload file

---
+++ Homework1.cs.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Code for Homework1
+        //The linear line equation is Z=4X^2 + 3Y; when X = 2.5, Y = 3.3
+        decimal X = 2.5m;
+        decimal Y = 3.3m;
+        decimal Z = (4*(X*X))+(3*Y);
+
+        Console.WriteLine("X = {0}, Y = {1}", X, Y);
+        Console.WriteLine("The value of Z is {0}", Z);
+
+
+    }
+}
